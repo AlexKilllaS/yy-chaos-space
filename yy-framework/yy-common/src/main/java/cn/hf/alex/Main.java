81183de87854faf9aef2526dd5950dde1e2343f0
yyhuangfu <hfyy0726@qq.com>
2023-05-20 03:16:01 +0800

    [init] init back end structure

---
+++ yy-framework/yy-common/src/main/java/cn/hf/alex/Main.java
@@ -0,0 +1,11 @@
+package cn.hf.alex;
+
+/**
+ * @author yyhuangfu
+ * @create 2023/5/20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
